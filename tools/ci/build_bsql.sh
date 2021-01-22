e456b68236d9bea52660819f8cf98933af31daba
MarinaGryphon <ryfoxx93@gmail.com>
2021-01-22 14:50:02 -0600

    Update build_bsql.sh

--- tools/ci/build_bsql.sh
+++ tools/ci/build_bsql.sh
@@ -8,10 +8,11 @@ cd BSQL
 
 mkdir -p artifacts
 cd artifacts
-export CXX=g++-7
+export CXX=g++
 # The -D will be unnecessary past BSQL v1.4.0.0
 cmake .. -DMARIA_LIBRARY=/usr/lib/i386-linux-gnu/libmariadb.so
 make
 
 mkdir -p ~/.byond/bin
 ln -s $PWD/src/BSQL/libBSQL.so ../../libBSQL.so
+ldd ../../libBSQL.so
