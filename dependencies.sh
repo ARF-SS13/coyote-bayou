41d521d15f8f806e2919defc27d489697f28816f
Loorey <loorey@disroot.org>
2022-01-29 21:06:25 +0000

    always use vendored nodejs and bump dependencies (#271)

--- dependencies.sh
+++ dependencies.sh
@@ -5,20 +5,20 @@
 
 # byond version
 export BYOND_MAJOR=514
-export BYOND_MINOR=1556
+export BYOND_MINOR=1571
 
 #rust_g git tag
 export RUST_G_VERSION=0.4.10
 
 #node version
-export NODE_VERSION=12
-export NODE_VERSION_PRECISE=12.22.4
+export NODE_VERSION=16
+export NODE_VERSION_PRECISE=16.13.2
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.7
 
 # Python version for mapmerge and other tools
-export PYTHON_VERSION=3.6.8
+export PYTHON_VERSION=3.7.9
 
 # Auxmos git tag
 export AUXMOS_VERSION=v0.2.3
