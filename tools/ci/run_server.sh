441e5b6a68925d4a335798d7d590d83e6eb1bca1
GremlingSS <gremlingthing@gmail.com>
2022-07-11 21:53:01 +0100

    aaaa

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -9,5 +9,10 @@ cp tools/ci/ci_config.txt ci_test/config/config.txt
 
 cd ci_test
 DreamDaemon fortune13.dmb -close -trusted -verbose -params "log-directory=ci"
+
 cd ..
+
+mkdir -p data/screenshots_new
+cp -r ci_test/data/screenshots_new data/screenshots_new
+
 cat ci_test/data/logs/ci/clean_run.lk
