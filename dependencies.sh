54acefe383d02bc822b44e78d52d4509ff366ef1
Dameon Owen <dameonowen@yahoo.com>
2024-03-10 00:16:26 -0500

    Update reloader and deps

--- dependencies.sh
+++ dependencies.sh
@@ -4,8 +4,8 @@
 #Final authority on what's required to fully build the project
 
 # byond version
-export BYOND_MAJOR=514
-export BYOND_MINOR=1589
+export BYOND_MAJOR=515
+export BYOND_MINOR=1633
 
 #rust_g git tag
 export RUST_G_VERSION=0.5.0
--- tgui/packages/tgui-dev-server/reloader.js
+++ tgui/packages/tgui-dev-server/reloader.js
@@ -67,7 +67,8 @@ export const findCacheRoot = async () => {
 
 const onCacheRootFound = cacheRoot => {
   logger.log(`found cache at '${cacheRoot}'`);
-  // Plant a dummy browser window file, we'll be using this to avoid world topic. For byond 514.
+  // Plant a dummy browser window file,
+  // we'll be using this to avoid world topic. For byond 514.
   fs.closeSync(fs.openSync(cacheRoot + '/dummy', 'w'));
 };
 
