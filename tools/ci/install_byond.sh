9567d9f0242b3d2ac262964f24253eae253b0867
Dameon Owen <dameonowen@yahoo.com>
2026-03-19 19:38:54 -0400

    Add debugging information

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -11,7 +11,9 @@ else
   rm -rf "$HOME/BYOND"
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
+  echo "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip"
   curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+  ls
   unzip byond.zip
   rm byond.zip
   cd byond
