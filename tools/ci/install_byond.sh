c23ca1c2937dc8f671e3e72d0e0d826bb73fb42c
Dameon Owen <dameonowen@yahoo.com>
2026-03-19 20:08:51 -0400

    Curl verbose flags

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -12,7 +12,7 @@ else
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
   echo "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip"
-  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+  curl -vvv "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
   ls
   unzip byond.zip
   rm byond.zip
